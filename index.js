6085f38f7cd1e90a0c00d348ca41eb6676c1e6d9
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-06-09 15:18:38 +0800

    Logged error properties

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var logger;
+'use strict';
+
 
 module.exports = function (_logger) {
-    logger = logger || _logger;
+
+	var logger = logger || _logger;
+
     return function (err, req, res, next) {
-        logger.log('error', err.message || err.data || err);
+    	var error = err.message || err.data || err,
+    		key;
+
+        logger.log('error', error);
+
+        for (key in error) {
+        	if (typeof(error[key]) !== 'function') {
+        		logger.log('warn', key + ': ' + error[key]);
+        	}
+        }
+
         if (err.stack) {
             logger.log('error', err.stack);
         }
+
         return res.status(err.statusCode || 400)
-                .send({message : err.message || err.data || err});
+                .send({message : error});
     };
 };
+
+
