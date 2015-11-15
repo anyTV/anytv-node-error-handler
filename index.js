077da6c16711b7c96e7eac6c62d3372b120f1de2
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-11-16 01:35:36 +0800

    Added readme and test cases

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
 'use strict';
 
-
-module.exports = function (_logger) {
-
-	var logger = logger || _logger;
-
-    return function (err, req, res, next) {
-    	var error = err.message || err.data || err,
-    		key;
-
-        if (!(err instanceof Error)) {
-            err = new Error(err);
-        }
-
-        logger.log('error', error);
-
-        if (typeof error === 'object') {
-	        for (key in error) {
-	        	if (typeof(error[key]) !== 'function') {
-	        		logger.log('warn', key + ': ' + JSON.stringify(error[key]));
-	        	}
-	        }
-        }
-
-        if (err.stack) {
-            logger.log('error', err.stack);
-        }
-
-        return res.status(err.statusCode || 500)
-                .send({message : error});
-    };
-};
-
-
+module.exports = require('./lib/index');
